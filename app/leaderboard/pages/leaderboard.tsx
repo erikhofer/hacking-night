dcc8026efaa8feb745ce90bdf2f3e0fa1698ab34
Erik Hofer <erik-hofer@mail.de>
2022-05-05 21:26:56 +0200

    feat: autoscrolling

--- app/leaderboard/pages/leaderboard.tsx
+++ app/leaderboard/pages/leaderboard.tsx
@@ -6,6 +6,7 @@ import Layout from "../../core/layouts/Layout"
 import getLeaderboardData from "../queries/getLeaderboardData"
 import type { Challenge } from "../queries/getLeaderboardData"
 import { Animator } from "@arwes/animation"
+import { position } from "polished"
 
 const LeaderboardPage: BlitzPage = () => (
   <Suspense>
@@ -16,6 +17,28 @@ const LeaderboardPage: BlitzPage = () => (
 const Leaderboard: React.FC = () => {
   const [data] = useQuery(getLeaderboardData, {}, { refetchInterval: 5000 })
   const columnWidths = ["20%", ...data.teams.map((t) => `${80 / data.teams.length}%`)]
+
+  useEffect(() => {
+    const max = Math.max(
+      document.body.scrollHeight,
+      document.body.offsetHeight,
+      document.documentElement.clientHeight,
+      document.documentElement.scrollHeight,
+      document.documentElement.offsetHeight
+    )
+    let curr = 0
+    let dir = 1
+    const interval = setInterval(() => {
+      if (curr >= max) {
+        dir = -1
+      } else if (curr <= 0) {
+        dir = 1
+      }
+      window.scrollTo({ top: curr, behavior: "smooth" })
+      curr += dir * 1
+    }, 100)
+    return () => clearInterval(interval)
+  }, [])
 
   // useEffect(() => {
   //   let timeout
@@ -45,6 +68,18 @@ const Leaderboard: React.FC = () => {
 
   return (
     <div>
+      <style>
+        {`
+        .arwes-table {
+          overflow: unset;
+        }
+        .arwes-table__row--header {
+          position: sticky;
+          top: 10px;
+          opacity: 1 !important;
+        }
+        `}
+      </style>
       <Table
         headers={headers}
         dataset={dataset}
